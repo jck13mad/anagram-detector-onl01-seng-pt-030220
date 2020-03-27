1da01dcc0adaeb02627a7b2aa34c5d3c013b97b5
Jack Madden <jck13mad@gmail.com>
2020-03-27 22:41:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,10 @@ class Anagram
     @word = word 
   end
   
-  def match(array_of_words)
-    array_of_words.select do |words|
-      (@words
+    def match(array_of_words)
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
   end
   
   
